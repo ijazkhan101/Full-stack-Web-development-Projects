8ba9e5aedc219cbeb5450c42aebf7e99bd489a2f
ijazdev101 <ijazkhan19932@gmail.com>
2023-04-15 00:11:51 +0500

    map
    map

--- Full Stack React js Projects by Mosh Hamedani/react-app/src/components/ListGroup.tsx
+++ Full Stack React js Projects by Mosh Hamedani/react-app/src/components/ListGroup.tsx
@@ -1,11 +1,23 @@
+import { Fragment } from "react";
 
 function ListGroup() {
-return (
-    <>
-    <h1>List Group</h1>
-    </>
-)
-}
+  const items = ["New York", "San Francisco", "Tokoyo", "London", "Paris"];
 
+  items.map((items) => <li></li>);
+  return (
+    <Fragment>
+      <ul className="list-group">
+        {items.map((item) => (
+          <li  className="list-group-item" key={item}>{items}</li>
+        ))}
+      </ul>
+      <ul className="list-group">
+        {items.map((item) => (
+          <li  className="list-group-item"  key={item}>{items}</li>
+        ))}
+      </ul>
+    </Fragment>
+  );
+}
 
 export default ListGroup;
